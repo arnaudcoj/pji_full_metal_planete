8ba7458ed6dd6dcf10fc300880ba48bd261529e1
Kaendan <tristan.camus@gmail.com>
2016-02-11 18:07:34 +0100

    Started to implement the interface: I draw an hexagon

--- full_metal_planete/app/main.cpp
+++ full_metal_planete/app/main.cpp
@@ -6,6 +6,26 @@ using namespace std;
 
 int main()
 {
+#include <SFML/Graphics.hpp>
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape hexagon(100, 6);
+    hexagon.setFillColor(sf::Color(255, 255, 255, 255));
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(hexagon);
+        window.display();
+    }
+
     Game game = Game();
     game.start();
     return 0;
