266a70b3b574106600deda99ee29df4107f62d53
Kaendan <tristan.camus@gmail.com>
2016-03-23 10:44:08 +0100

    Changed the pawn size

--- interface/pawn.cpp
+++ interface/pawn.cpp
@@ -36,7 +36,7 @@ Pawn::Pawn(std::shared_ptr<Piece> const& piece)
     }
 
     m_sprite.setOrigin(m_size.x / 2, m_size.y / 2);
-    m_sprite.setScale(4, 4);
+    m_sprite.setScale(2, 2);
 
     auto& animation_idle = m_animator.CreateAnimation("idle", m_piece->getType(), sf::seconds(0.2), true);
     animation_idle.AddFrames(sf::Vector2i(0, 0), m_size, 1);
